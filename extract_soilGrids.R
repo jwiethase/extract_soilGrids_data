27607086250521af06306ca3046fc83e9e351e0c
Joris Wiethase <joriswiethase@nas-10-240-191-164.york.ac.uk>
2019-12-03 15:53:57 +0000

    Calculate field capacity from published variables, get soil variables for York

--- extract_soilGrids.R
+++ extract_soilGrids.R
@@ -13,7 +13,7 @@ suppressWarnings(suppressMessages(suppressPackageStartupMessages({
   library(raster)
   library(ggplot2)
   library(ggthemes)
-  library(dplyr)
+  library(tidyverse)
 })))
 
 # Set global ggplot theme
@@ -22,19 +22,19 @@ theme_set(ggthemes::theme_few(base_size = 10))
 # Set working directory
 setwd("~/Google Drive/Work/PhD_York/site_data/soilGrids/extract_soilGrids_data/")
 
------
+
 #'## Data import and preparation
 #' Import soil files, downloaded from https://soilgrids.org/ for 15cm depth
-soil_sand <- raster("soil_files/SNDPPT_M_sl3_250m.tif")
-soil_silt <- raster("soil_files/SLTPPT_M_sl3_250m.tif")
-soil_clay <- raster("soil_files/CLYPPT_M_sl3_250m.tif")
-soil_cec <- raster("soil_files/CECSOL_M_sl3_250m.tif")
-soil_bld <- raster("soil_files/BLDFIE_M_sl3_250m.tif")
-soil_orc <- raster("soil_files/ORCDRC_M_sl3_250m.tif")
-soil_phi <- raster("soil_files/PHIHOX_M_sl3_250m.tif")
+soil_sand <- raster("soil_files_TZ/SNDPPT_M_sl3_250m.tif")
+soil_silt <- raster("soil_files_TZ/SLTPPT_M_sl3_250m.tif")
+soil_clay <- raster("soil_files_TZ/CLYPPT_M_sl3_250m.tif")
+soil_cec <- raster("soil_files_TZ/CECSOL_M_sl3_250m.tif")
+soil_bld <- raster("soil_files_TZ/BLDFIE_M_sl3_250m.tif")
+soil_orc <- raster("soil_files_TZ/ORCDRC_M_sl3_250m.tif")
+soil_phi <- raster("soil_files_TZ/PHIHOX_M_sl3_250m.tif")
 
 
------
+
 #'## Data extraction
 #' Create grid to extract data from raster
 # Use metric CRS to space out grid equal to soil data resolution (250m). Use extent of pre-defined,
@@ -59,9 +59,9 @@ extr_df$sand <- raster::extract(soil_sand, grd_WGS84)
 extr_df$silt <- raster::extract(soil_silt, grd_WGS84)
 extr_df$clay <- raster::extract(soil_clay, grd_WGS84)
 extr_df$cec <- raster::extract(soil_cec, grd_WGS84)
-extr_df$bld <- raster::extract(soil_bld, grd_WGS84)
+extr_df$bld <- raster::extract(soil_bld, grd_WGS84)   # in kg/m3
 extr_df$orc <- raster::extract(soil_orc, grd_WGS84)
-extr_df$phi <- raster::extract(soil_phi, grd_WGS84)
+extr_df$phi <- raster::extract(soil_phi, grd_WGS84)*0.1
 
 #' Replace missing values:
 extr_df$bldf <- ifelse(is.na(extr_df$bld),
@@ -69,14 +69,14 @@ extr_df$bldf <- ifelse(is.na(extr_df$bld),
 
 #' Calculate water holding capacity
 extr_df <- cbind(extr_df, GSIF::AWCPTF(extr_df$sand, extr_df$silt,
-                                       extr_df$clay, extr_df$orc, BLD=extr_df$bldf*1000, extr_df$cec,
+                                       extr_df$clay, extr_df$orc, BLD=extr_df$bldf, extr_df$cec,
                                        extr_df$phi, h1=-10, h2=-20, h3=-31.6)) %>% 
   mutate(field_capacity = AWCh1 + WWP)
 
 # Save the created data frame
 # write.csv(extr_df, "extracted_soil.csv")
 
------
+
   #'## Visually explore the data
   #' Plot density of soil properties for the chosen area
 ggplot() +
@@ -103,13 +103,98 @@ ggplot() +
              col = "red", alpha = .5, lty = 2) +
   xlab("Weight percentage of the clay particles at 15cm depth (%)") 
 
------
+
 #'## Summarize the soil values
 soil_average <- extr_df %>% 
   dplyr::summarise_all(list(av = median)) %>% 
   mutate_if(is.numeric, ~round(., 3))
 print(soil_average)
 
-solomon_cult3 <- GSIF::AWCPTF(SNDPPT=60.6, SLTPPT=13.6, CLYPPT=25.7, 
-             ORCDRC=8.3, BLD=1.33*1000, CEC=14.1, PHIHOX=7, h1=-10, h2=-20, h3=-31.6)
-solomon_cult3$field_capacity = solomon_cult3$AWCh1 + solomon_cult3$WWP
+
+#'## Compare the extracted soil values with values from spatially close (~85km) field study
+solomon <- data.frame("Site" = c("wood_nat", "wood_degr", "cult3", "cult15"),
+                      "SNDPPT" = c(50.4, 69.7, 60.6, 60.4),
+                      "SLTPPT" = c(19.4, 11.6, 13.6, 13.5),
+                      "CLYPPT" = c(30.2, 18.6, 25.7, 26.1),
+                      "ORCDRC" = c(18.7, 13.8, 8.3, 8.2),
+                      "BLD" = c(1.2, 1.33, 1.33, 1.33)*1000,  # Convert g/cm3 to kg/m3
+                      "CEC" = c(16.8, 15.7, 14.1, 11.7),
+                      "PHIHOX" = c(6.6, 6.9, 7, 6.7))
+
+solomon_soil <- solomon %>% 
+  group_by(Site) %>% 
+  do(new = GSIF::AWCPTF(SNDPPT = .$SNDPPT, SLTPPT = .$SLTPPT, CLYPPT = .$CLYPPT, 
+                        ORCDRC= .$ORCDRC, BLD= .$BLD, CEC =.$CEC, 
+                        PHIHOX = .$PHIHOX,
+                        h1=-10, h2=-20, h3=-31.6)) %>% 
+  unnest() %>% 
+  mutate(field_capacity = AWCh1 + WWP)
+  
+t.test(solomon_soil$field_capacity, extr_df$field_capacity) 
+
+
+#'## Extract soil values or York (soil that will be used to prepare mix for study)
+soil_sand2 <- raster("soil_files_York/SNDPPT_M_sl3_250m.tif")
+soil_silt2 <- raster("soil_files_York/SLTPPT_M_sl3_250m.tif")
+soil_clay2 <- raster("soil_files_York/CLYPPT_M_sl3_250m.tif")
+soil_cec2 <- raster("soil_files_York/CECSOL_M_sl3_250m.tif")
+soil_bld2 <- raster("soil_files_York/BLDFIE_M_sl3_250m.tif")
+soil_orc2 <- raster("soil_files_York/ORCDRC_M_sl3_250m.tif")
+soil_phi2 <- raster("soil_files_York/PHIHOX_M_sl3_250m.tif")
+  
+grd2 <- expand.grid(x = seq(from = 622618, to = 633896,
+                           by = 250),
+                   y = seq(from = 5965695, 
+                           to = 5986171, 
+                           by = 250))
+sp::coordinates(grd2) <- ~x + y
+sp::gridded(grd2) <- TRUE
+
+# Convert CRS to WGS84 for value extraction
+sp::proj4string(grd2) <-  CRS("+init=epsg:32630")
+grd2_WGS84 <- spTransform(grd2, CRS("+init=epsg:4326"))
+grd2_WGS84 <- as.data.frame(grd2_WGS84)
+
+#' Extract soil values from tifs using the grid points
+extr_df2 <- grd2_WGS84
+
+extr_df2$sand <- raster::extract(soil_sand2, grd2_WGS84)
+extr_df2$silt <- raster::extract(soil_silt2, grd2_WGS84)
+extr_df2$clay <- raster::extract(soil_clay2, grd2_WGS84)
+extr_df2$cec <- raster::extract(soil_cec2, grd2_WGS84)
+extr_df2$bld <- raster::extract(soil_bld2, grd2_WGS84)
+extr_df2$orc <- raster::extract(soil_orc2, grd2_WGS84)
+extr_df2$phi <- raster::extract(soil_phi2, grd2_WGS84)*0.1
+
+#' Replace missing values:
+extr_df2$bldf <- ifelse(is.na(extr_df2$bld),
+                       mean(extr_df2$bld, na.rm=TRUE), extr_df2$bld)
+
+#' Calculate water holding capacity
+extr_df2 <- cbind(extr_df2, GSIF::AWCPTF(extr_df2$sand, extr_df2$silt,
+                                       extr_df2$clay, extr_df2$orc, BLD=extr_df2$bldf, extr_df2$cec,
+                                       extr_df2$phi, h1=-10, h2=-20, h3=-31.6)) %>% 
+  mutate(field_capacity = AWCh1 + WWP)
+  
+#'## Summarize the soil values
+soil_average2 <- extr_df2 %>% 
+  dplyr::summarise_all(list(av = median)) %>% 
+  mutate_if(is.numeric, ~round(., 3))
+
+print(soil_average2)
+soil_comp <- rbind(soil_average, soil_average2)  
+soil_comp$Site <- c("TZ", "York") 
+soil_comp <- soil_comp %>% 
+  dplyr::select(-c(x_av, y_av, tetaS_av, AWCh3_av, AWCh2_av)) %>% 
+  dplyr::select(Site, everything())
+
+
+soil_average2
+GSIF::AWCPTF(SNDPPT = 38, SLTPPT = 21.7, CLYPPT = 40.3, 
+             ORCDRC= 2.84, BLD= 1230, CEC =20.4, 
+             PHIHOX = 5.61,
+             h1=-10, h2=-20, h3=-31.6)["AWCh1"] + GSIF::AWCPTF(SNDPPT = 38, SLTPPT = 21.7, CLYPPT = 40.3, 
+                                                               ORCDRC= 3.3, BLD= 1200, CEC =20.4, 
+                                                               PHIHOX = 4.8,
+                                                               h1=-10, h2=-20, h3=-31.6)["WWP"]
+
